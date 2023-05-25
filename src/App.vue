bbb795d689c9eee224aa86bde6928c3c7d6bd83c
mahaitao <mahaitao1120@gmail.com>
2023-05-25 16:55:55 +0800

    fix: 代码更新

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <!-- <mTest/> -->
   </div>
 </template>
 
 <script>
+// import mhtPlugin from 'mht-plugin'
+// console.log(test.sum2, mTest)
+// let { mTest } = mhtPlugin
+// console.log(mhtPlugin)
+// console.log(window)
 export default {
   name: 'app',
   data () {
     return {
     }
+  },
+  components: {
+    // mTest
   }
 }
 </script>
